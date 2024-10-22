98b96789038895f3cf8d4af361abfce3aae9be60
Buster <yahyemohamed2002@gmail.com>
2024-10-21 23:44:07 -0500

    created index and connection files to do some quering with the database and connect it

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,13 +10,13 @@ VALUES  (011, 'Team Lead', 200000, 004),
         (012, 'Senior Engineer', 175000, 001),
         (013, 'Junior Engineer' 125000, 003),
         (014, 'Manager', 250000, 002),
-        (015, 'Mid-level Engineer', 150000, 005),
+        (015, 'Mid-level Engineer', 150000, 002),
 
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (021, 'Doe', 'Mo', 011),
+VALUES  (021, 'Doe', 'Mo', 011,),
         (002, 'John', 'Doe', 012),
         (003, 'Ron', 'Soe', 013),
         (004, 'Mon', 'Goe', 014),
-        (004, 'Eon', 'Loe', 015),
+        (004, 'Eon', 'Loe', 015, 021),
         
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import pg from 'pg';
+import { pool, connectToDb } from './connection.js';
