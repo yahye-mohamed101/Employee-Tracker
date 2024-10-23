b9b717e5ba88ca2c0a2d4ef0d903651ae24403cd
Yahya Mohamed <yahyemohamed2002@gmail.com>
2024-10-23 16:26:54 -0500

    fixed schema file

--- db/schema.sql
+++ db/schema.sql
@@ -13,8 +13,9 @@ CREATE TABLE role (
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
+
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
@@ -22,9 +23,10 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
+
 
 \d department
 \d employee
